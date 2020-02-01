9e3a2c8e102514646d420b9f5139279d917540c6
Joey Meyer <joeymeyer1986@gmail.com>
2020-01-31 23:52:44 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OceanCleaner
+
+Developed with Unreal Engine 4
---
+++ Source/OceanCleaner/OceanCleanerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OceanCleanerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OCEANCLEANER_API AOceanCleanerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
